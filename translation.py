6d6acd231a1793805f8032c962857b6c79d89795
SUSHANT GIRDHAR <52759137+sushantgirdhar@users.noreply.github.com>
2020-12-19 22:27:44 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
 
       # Start text
-      START = "Hi I am a channel auto forward bot made by <a href='https://t.me/ebruiser'>Channel Automatic</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @ebruiser ,"             
+      START = "Hi I am a channel auto forward bot made by <a href='https://t.me/ebruiser'>Channel Automatic</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel </b>\n @ebruiser ."             
 
       #About text
       ABOUT = """
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
 <b>👨‍💻 Developer:</b> ebruiser
-<b>📢 Channel:</b> NS BOT UPDATES
-<b>👥 Group:</b> Ns BOT SUPPORT
-<b>🌐Source Code:</b> Press Me 🥰"""
+<b>📢 Channel:</b> @EBRUISER
+<b>👥 Group:</b> @EBRUISER
+<b>🌐Source Code:</b>PRIVATE 🥰"""
